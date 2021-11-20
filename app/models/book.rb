25a5ae6146647eea82b2d9d08a0d367dad46f168
dajhely angles <assiaxramx10@gmail.com>
2021-11-20 15:43:17 -0500

    Adding relations and presence validations to Book & Review model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
+  belongs_to :user
   has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :slug, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :book
+  belongs_to :review
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
